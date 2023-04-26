049c2ab10a3ee9f8511061b9b4d5117ede0963c3
Astor Almeida <astormg@gmail.com>
2023-04-26 12:00:45 -0300

    feat: adicionando o app

---
+++ app-web-adocao/app-web-adocao/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace app_web_adocao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

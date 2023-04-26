4d9d60c2a3f473675af34311361758049af9a878
Astor Almeida <astormg@gmail.com>
2023-04-26 12:16:44 -0300

    feat: Criação do ApplicationDbContext

---
+++ app-web-adocao/app-web-adocao/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_adocao.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+    }
+}

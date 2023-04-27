8011addd904cac55ab1494ecf6809a17a1469c22
Astor Almeida <astormg@gmail.com>
2023-04-27 13:54:11 -0300

    feat: Atualizando Alterações na tentativa de Incluir um Controlador MVC com View

--- app-web-adocao/app-web-adocao/Startup.cs
+++ app-web-adocao/app-web-adocao/Startup.cs
@@ -1,9 +1,9 @@
 using app_web_adocao.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;

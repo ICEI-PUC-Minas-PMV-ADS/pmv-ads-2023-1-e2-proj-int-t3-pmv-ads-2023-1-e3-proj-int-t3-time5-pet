80e4ab04239ab40a022445879570ce7ed474ee0e
Astor Almeida <astormg@gmail.com>
2023-04-27 21:02:56 -0300

    feat: Inclusão dos Controladores

---
+++ app-adocao/app-adocao/Controllers/RequerentesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using app_adocao.Models;
+
+namespace app_adocao.Controllers
+{
+    public class RequerentesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RequerentesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Requerentes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Requerentes != null ? 
+                          View(await _context.Requerentes.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Requerentes'  is null.");
+        }
+
+        // GET: Requerentes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Requerentes == null)
+            {
+                return NotFound();
+            }
+
+            var requerente = await _context.Requerentes
+                .FirstOrDefaultAsync(m => m.Login == id);
+            if (requerente == null)
+            {
+                return NotFound();
+            }
+
+            return View(requerente);
+        }
+
+        // GET: Requerentes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requerentes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Tipo,Raca,Login,Nome,Senha,Cidade,Estado")] Requerente requerente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(requerente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requerente);
+        }
+
+        // GET: Requerentes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Requerentes == null)
+            {
+                return NotFound();
+            }
+
+            var requerente = await _context.Requerentes.FindAsync(id);
+            if (requerente == null)
+            {
+                return NotFound();
+            }
+            return View(requerente);
+        }
+
+        // POST: Requerentes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Tipo,Raca,Login,Nome,Senha,Cidade,Estado")] Requerente requerente)
+        {
+            if (id != requerente.Login)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(requerente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequerenteExists(requerente.Login))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requerente);
+        }
+
+        // GET: Requerentes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Requerentes == null)
+            {
+                return NotFound();
+            }
+
+            var requerente = await _context.Requerentes
+                .FirstOrDefaultAsync(m => m.Login == id);
+            if (requerente == null)
+            {
+                return NotFound();
+            }
+
+            return View(requerente);
+        }
+
+        // POST: Requerentes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Requerentes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Requerentes'  is null.");
+            }
+            var requerente = await _context.Requerentes.FindAsync(id);
+            if (requerente != null)
+            {
+                _context.Requerentes.Remove(requerente);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequerenteExists(string id)
+        {
+          return (_context.Requerentes?.Any(e => e.Login == id)).GetValueOrDefault();
+        }
+    }
+}
